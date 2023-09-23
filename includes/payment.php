970b8c8c71c7f328cc39cf6601ed2f9fe883fcbe
Amogh Bansal <bansalamogh0@gmail.com>
2023-09-23 17:22:16 +0530

    Add files via upload

---
+++ includes/payment.php
@@ -0,0 +1,6 @@
+<div>
+    <h2 align="center" style="color:blue">Pay now with Paytm:</h2>
+    <img src="includes/paytm.jpg" alt="" height="330px" width="200px">
+    <h3 style="color:red">Then attach the screenshot and mail it to me!</h3>
+    <h4 style="color:orange">All other information about the packages would be informed to you through mail!</h4>
+</div>
